b8a47c6b4f89c5af1a557b1a42e18b30a0ce93c3
Rashmiya <rashmiyarafeek1200@gmail.com>
2025-05-11 21:34:52 +0530

    added SignInService endpoints

---
+++ src/services/SignInServices.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import useFetch from "../hooks/useFetch";
+
+const SignInServices = () => {
+  const { fetchAction } = useFetch();
+
+  const handleResponse = (response) => {
+    if (response?.success) {
+      return { responseType: "success", output: response };
+    } else {
+      return { responseType: "fail", output: response };
+    }
+  };
+
+  const loginUser = async (data) => {
+    try {
+      const response = await fetchAction({
+        query: "/customer/signin",
+        body: data,
+      });
+      return handleResponse(response);
+    } catch (error) {
+      return { responseType: "error", output: error };
+    }
+  };
+
+  const signUpUser = async (data) => {
+    try {
+      const response = await fetchAction({
+        query: "/customer/signup",
+        body: data,
+      });
+      return handleResponse(response);
+    } catch (error) {
+      return { responseType: "error", output: error };
+    }
+  };
+
+  const logOutUser = async (data) => {
+    try {
+      const response = await fetchAction({
+        query: "/customer/logout",
+        method: "get",
+      });
+      return handleResponse(response);
+    } catch (error) {
+      return { responseType: "error", output: error };
+    }
+  };
+
+  return { loginUser, signUpUser, logOutUser };
+};
+
+export default SignInServices;

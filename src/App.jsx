0288fb76b0fd6d043a9b3b7624d453011cc3fe98
Rashmiya <rashmiyarafeek1200@gmail.com>
2025-05-11 22:19:47 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { ConfigProvider } from 'antd';
+import { BrowserRouter } from 'react-router-dom';
+import Routerset from './routes/Routerset';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const containerStyle = {
+    width: '100%',
+    maxWidth: '100vw',
+    minHeight: '100vh',
+    backgroundColor: '#FFFFFF',
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <ConfigProvider
+      theme={{
+        token: {
+          fontFamily: 'Inter-Regular',
+        },
+        components: {
+          Notification: {
+            paddingContentHorizontalLG: 12,
+            paddingMD: 6,
+            paddingLG: 12,
+            fontSizeLG: 14,
+            controlHeightLG: 40,
+            colorBgElevated: '#36AE42',
+            colorIcon: 'rgb(255, 255, 255)',
+            colorTextHeading: 'rgba(254, 254, 254, 0.88)',
+            colorIconHover: 'rgb(255, 255, 255)',
+          },
+          Spin: {
+            colorPrimary: 'var(--color-green)',
+          },
+          Popover: {
+            colorTextHeading: 'var(--primary)',
+          },
+          Input: {
+            fontSizeSM: 10,
+            fontSize: 12,
+            fontSizeLG: 14,
+            autoComplete: 'off',
+          },
+          InputNumber: {
+            fontSizeSM: 10,
+            fontSize: 14,
+            fontSizeLG: 14,
+          },
+          DatePicker: {
+            fontSizeSM: 10,
+            fontSize: 12,
+            fontSizeLG: 14,
+          },
+          Select: {
+            fontSizeSM: 10,
+            fontSize: 12,
+            fontSizeLG: 14,
+          },
+          Tabs: {
+            colorText: 'white',
+            itemActiveColor: 'red',
+          },
+          Form: {
+            fontSizeSM: 10,
+            fontSize: 12,
+            fontSizeLG: 14,
+          },
+        },
+      }}
+    >
+      <div style={containerStyle}>
+        <BrowserRouter>
+          <Routerset />
+        </BrowserRouter>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </ConfigProvider>
+  );
 }
 
-export default App
+export default App;

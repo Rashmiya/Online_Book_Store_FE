2a48b06ee1be9874154952d9a1c2eefb2398d935
Rashmiya <rashmiyarafeek1200@gmail.com>
2025-05-11 21:39:25 +0530

    defined basic routes

---
+++ src/routes/Routerset.jsx
@@ -0,0 +1,87 @@
+import React, { useContext } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+import Dashboard from "../pages/Dashboard/Dashboard";
+import SignInView from "../pages/OnBoarding/Sign in/SignInView";
+import DashboardLayout from "../pages/Layout/DashboardLayout";
+import SignUpView from "../pages/OnBoarding/Sign up/SignUpView";
+import OnBoardingProcess from "../pages/OnBoarding/OnBoardingProcess";
+import AllBooks from "../pages/AllBooks/AllBooks";
+import BookView from "../pages/BookView/BookView";
+import HelpCenter from "../pages/LandingPages/HelpCenter";
+import BookCheckout from "../pages/BookCheckout/BookCheckout";
+import AboutUs from "../pages/LandingPages/AboutUs";
+import PrivacyPolicy from "../pages/LandingPages/PrivacyPolicy";
+import Contact from "../pages/LandingPages/Contact";
+import OrderSuccess from "../pages/OrderSuccess/OrderSuccess";
+
+const Routerset = () => {
+  const { token } = useContext(AuthContext);
+
+  return (
+    <Routes>
+      {/* -----------------------------basic routes----------------------------- */}
+      <Route path="onboarding/*" element={<OnBoardingProcess />}>
+        <Route path="" element={<SignInView />} />
+        <Route path="sign-in" element={<SignInView />} />
+        <Route path="sign-in/:id" element={<SignInView />} />
+        <Route path="sign-up" element={<SignUpView />} />
+        <Route path="sign-up/:id" element={<SignUpView />} />
+      </Route>
+
+      {/* -----------------------------private & public routes----------------------------- */}
+      {true ? (
+        <Route path="/" element={<DashboardLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="all-books" element={<AllBooks />} />
+          <Route path="view-book/:id" element={<BookView />} />
+          <Route path="help" element={<HelpCenter />} />
+          <Route path="aboutus" element={<AboutUs />} />
+          <Route path="privacypolicy" element={<PrivacyPolicy />} />
+          <Route path="contact" element={<Contact />} />
+          <Route
+            path="checkout-book/:id"
+            element={<PrivateRoute element={<BookCheckout />} />}
+          />
+          <Route
+            path="order-success/:id"
+            element={<PrivateRoute element={<OrderSuccess />} />}
+          />
+        </Route>
+      ) : (
+        <Route path="*" element={<SignInView />} />
+      )}
+    </Routes>
+  );
+};
+
+export default Routerset;
+
+const PublicRoutes = ({ element, allow }) => {
+  const { block, user } = useContext(AuthContext);
+
+  if (block) {
+    if (allow) {
+      if (user?.role === 3) {
+        return element;
+      } else {
+        // return <Navigate to="/unauthorized-error" replace />;
+        return element;
+      }
+    } else {
+      return <Navigate to="/subscription-expired" replace />;
+    }
+  } else {
+    return element;
+  }
+};
+
+const PrivateRoute = ({ element }) => {
+  const { isAuthChack, setIsAuthChack } = useContext(AuthContext);
+
+  if (isAuthChack) {
+    return element;
+  } else {
+    return <Navigate to="/onboarding/sign-in" replace />;
+  }
+};

0370ec384f5cb75810ab3c409fd5c7ea8b43c7a5
Rashmiya <rashmiyarafeek1200@gmail.com>
2025-05-11 22:25:44 +0530

    format updated

--- src/routes/Routerset.jsx
+++ src/routes/Routerset.jsx
@@ -1,19 +1,19 @@
-import React, { useContext } from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
-import { AuthContext } from "../context/AuthContext";
-import Dashboard from "../pages/Dashboard/Dashboard";
-import SignInView from "../pages/OnBoarding/Sign in/SignInView";
-import DashboardLayout from "../pages/Layout/DashboardLayout";
-import SignUpView from "../pages/OnBoarding/Sign up/SignUpView";
-import OnBoardingProcess from "../pages/OnBoarding/OnBoardingProcess";
-import AllBooks from "../pages/AllBooks/AllBooks";
-import BookView from "../pages/BookView/BookView";
-import HelpCenter from "../pages/LandingPages/HelpCenter";
-import BookCheckout from "../pages/BookCheckout/BookCheckout";
-import AboutUs from "../pages/LandingPages/AboutUs";
-import PrivacyPolicy from "../pages/LandingPages/PrivacyPolicy";
-import Contact from "../pages/LandingPages/Contact";
-import OrderSuccess from "../pages/OrderSuccess/OrderSuccess";
+import React, { useContext } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
+import Dashboard from '../pages/Dashboard/Dashboard';
+import SignInView from '../pages/OnBoarding/Sign in/SignInView';
+import DashboardLayout from '../pages/Layout/DashboardLayout';
+import SignUpView from '../pages/OnBoarding/Sign up/SignUpView';
+import OnBoardingProcess from '../pages/OnBoarding/OnBoardingProcess';
+import AllBooks from '../pages/AllBooks/AllBooks';
+import BookView from '../pages/BookView/BookView';
+import HelpCenter from '../pages/LandingPages/HelpCenter';
+import BookCheckout from '../pages/BookCheckout/BookCheckout';
+import AboutUs from '../pages/LandingPages/AboutUs';
+import PrivacyPolicy from '../pages/LandingPages/PrivacyPolicy';
+import Contact from '../pages/LandingPages/Contact';
+import OrderSuccess from '../pages/OrderSuccess/OrderSuccess';
 
 const Routerset = () => {
   const { token } = useContext(AuthContext);

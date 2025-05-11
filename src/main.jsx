bbf6eee89425b1b27d327d1650d9508f4f547a8d
Rashmiya <rashmiyarafeek1200@gmail.com>
2025-05-11 21:55:17 +0530

    added providers to main file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { AuthProvider } from './context/AuthContext.jsx';
+import { NotificationContextProvider } from './context/NotificationContext.jsx';
+import { HomeAndNotificationContextProvider } from './context/HomeAndNotificationContext.jsx';
+import { BookContextProvider } from './context/BookContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AuthProvider>
+      <NotificationContextProvider>
+        <HomeAndNotificationContextProvider>
+          <BookContextProvider>
+            <App />
+          </BookContextProvider>
+        </HomeAndNotificationContextProvider>
+      </NotificationContextProvider>
+    </AuthProvider>
+  </StrictMode>
+);
